8ad495dce45c0d052da609dd47759d4a4245c1d3
Damodharan <damodharan.kothandaraman@ncsc.go.bh>
2022-10-04 13:37:40 +0300

    Grid View Layout

--- lib/pages/launcher.dart
+++ lib/pages/launcher.dart
@@ -1,60 +1,102 @@
 import 'package:flutter/material.dart';
-import 'package:my_sample_app/pages/dashboard.dart';
 
+class LauncherPage extends StatefulWidget  {
+  const LauncherPage({ Key? key }) : super(key: key);
 
-class LauncherPage extends StatelessWidget  {
-  const LauncherPage({super.key});
-  @override
+@override
+  _LauncherPageState createState() => _LauncherPageState();
+  
+}
+
+class _LauncherPageState  extends State <LauncherPage>{
+
+  List<CountryName> countryNames = [
+    CountryName('One','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Two','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Three','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Four','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Five','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Six','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Seven','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Eight','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Nine','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+    CountryName('Ten','https://thumbs.dreamstime.com/b/tick-icon-circle-symbol-flat-solution-check-approval-128428629.jpg'),
+  ];
+
+int selectedService = 4;
+
+ @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body:  Center(
-          child: Container(
-            margin: const EdgeInsets.all(10),
-            color: Colors.amber[600],
-             width: 108.0,
-            height: 48.0,
-            child: MaterialButton(
-                      elevation: 0,
-                      color: Colors.black,
-                      onPressed: () {
-                        _gotoDetailsPage(context);
-                        
-                      },
-                      height: 55,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'Click Me',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                    ),
-            
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 80),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            height: MediaQuery.of(context).size.height * 0.45,
+            width: MediaQuery.of(context).size.width,
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 6,
+                childAspectRatio: 2.0,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 4.0,), 
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: countryNames.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(1)
+                  ),
+                  child: Text(countryNames[index].name),
+                );
+               }),
+          )
+        ],
+      )
+    );
+  }
+  countryContainer(String image, String name, int index) {
+    return GestureDetector(
+      onTap: () {},
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: selectedService == index ? Colors.white : Colors.grey.shade100,
+          border: Border.all(
+            color: selectedService == index
+                ? Colors.blue.shade100
+                : Colors.grey.withOpacity(0),
+            width: 2.0,
           ),
+          borderRadius: BorderRadius.circular(15.0),
         ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network(image, height: 30),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                name,
+                style: const TextStyle(fontSize: 14),
+              )
+            ]),
       ),
     );
   }
+
+
 }
 
- void _gotoDetailsPage(BuildContext context) {
-   Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => DashboardPage(),
-    ),
-  );
- }
+class CountryName {
+  final String name;
+  final String imageURL;
+  CountryName(this.name, this.imageURL);
+}
 
 
